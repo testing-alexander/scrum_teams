0d22f0708a84abfaa2697b608841558e4b9fc5c7
marithzacasta <marithzacastano9.5@gmail.com>
2024-08-06 10:26:32 -0500

    Se le añadio color y emois al codigo

--- scrum.py
+++ scrum.py
@@ -1,28 +1,32 @@
 import random
+import emoji
+from colorama import Fore, Style, init
+
+# Inicializa colorama
+init(autoreset=True)
 
 def adivina_el_numero():
     numero_secreto = random.randint(1, 100)
     intentos = 0
-    print("¡Bienvenido al juego de Adivina el Número!")
-    print("Estoy pensando en un número entre 1 y 100.")
+    print(Fore.GREEN + "¡Bienvenido al juego de Adivina el Número!")
+    print(Fore.GREEN + "Estoy pensando en un número entre 1 y 100.")
 
     while True:
         try:
-            adivinanza = int(input("Adivina el número: "))
+            adivinanza = int(input(Fore.CYAN + "Adivina el número: "))
             intentos += 1
 
             if adivinanza < numero_secreto:
-                print("Demasiado bajo. Intenta de nuevo.")
+                print(Fore.YELLOW + "Demasiado bajo. Intenta de nuevo.")
             elif adivinanza > numero_secreto:
-                print("Demasiado alto. Intenta de nuevo.")
+                print(Fore.YELLOW + "Demasiado alto. Intenta de nuevo.")
             else:
-                print(f"¡¡Felicidades!! Adivinaste el número en {intentos} intentos.")
+                # Usa emoji para la felicitación
+                print(Fore.CYAN + f"{emoji.emojize(':trophy:')} ¡¡Felicidades!! Adivinaste el número en {intentos} intentos.")
                 break
 
         except ValueError:
-            print("Por favor, ingresa un número válido.")
+            print(Fore.RED + "Por favor, ingresa un número válido.")
 
 if __name__ == "__main__":
     adivina_el_numero()
-
-
